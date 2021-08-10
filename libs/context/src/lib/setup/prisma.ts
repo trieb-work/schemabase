9324552acc5443b722d81679d17aaaf6438e60ab
Andreas Thomas <dev@chronark.com>
2021-08-10 12:24:46 +0200

    fix: add non-pooled database url

--- libs/context/src/lib/setup/prisma.ts
+++ libs/context/src/lib/setup/prisma.ts
@@ -5,5 +5,16 @@ import { PrismaClient } from "@eci/data-access/prisma"
  * Initialize a prisma client and make it public to the context
  */
 export const setupPrisma = (): ExtendContextFn<"prisma"> => async (ctx) => {
-  return { ...ctx, prisma: new PrismaClient() }
+  const url = process.env["DATABASE_URL_POOL"]
+  if (!url) {
+    throw new Error(`DATABASE_URL_POOL environment variable is undefined`)
+  }
+  return {
+    ...ctx,
+    prisma: new PrismaClient({
+      datasources: {
+        db: { url },
+      },
+    }),
+  }
 }
