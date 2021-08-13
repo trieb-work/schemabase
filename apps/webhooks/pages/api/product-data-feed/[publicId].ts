6bdab69db2cf596ff336f317f1c675722580b66b
Andreas Thomas <dev@chronark.com>
2021-08-13 10:27:17 +0200

    feat: validate request

--- apps/webhooks/pages/api/product-data-feed/[publicId].ts
+++ apps/webhooks/pages/api/product-data-feed/[publicId].ts
@@ -30,55 +30,62 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> {
-  const {
-    headers: {
-      "x-saleor-domain": domain,
-      "x-saleor-event": event,
-      "x-saleor-signature": signature,
-      "x-saleor-token": token,
-    },
-    body: { app_token: appToken },
-    query: { publicId, variant },
-  } = requestValidation.parse(req);
+  try {
+    const {
+      headers: {
+        "x-saleor-domain": domain,
+        "x-saleor-event": event,
+        "x-saleor-signature": signature,
+        "x-saleor-token": token,
+      },
+      body: { app_token: appToken },
+      query: { publicId, variant },
+    } = requestValidation.parse(req);
 
-  const ctx = await createContext<
-    "prisma" | "requestDataFeed" | "logger" | "saleor"
-  >(
-    setupPrisma(),
-    setupLogger(),
-    setupRequestDataFeed({ publicId, variant }),
-    setupSaleor({
-      domain,
-      event,
-      appToken,
-      authToken: token.replace("Bearer ", ""),
-      signature: signature.replace("sha1=", ""),
-    }),
-  );
+    const ctx = await createContext<
+      "prisma" | "requestDataFeed" | "logger" | "saleor"
+    >(
+      setupPrisma(),
+      setupLogger(),
+      setupRequestDataFeed({ publicId, variant }),
+      setupSaleor({
+        domain,
+        event,
+        appToken,
+        authToken: token.replace("Bearer ", ""),
+        signature: signature.replace("sha1=", ""),
+      }),
+    );
 
-  if (!ctx.requestDataFeed.valid) {
-    ctx.logger.error("Invalid request");
-    return res.status(400).end();
+    if (!ctx.requestDataFeed.valid) {
+      ctx.logger.error("Invalid request");
+      return res.status(400).end();
+    }
+
+    ctx.logger.info("Creating new product datafeed");
+
+    const productDataFeed = await generateProductDataFeed(
+      ctx.saleor.graphqlClient,
+      "FIXME: channel",
+      ctx.requestDataFeed.storefrontProductUrl,
+      ctx.requestDataFeed.variant,
+    );
+
+    if (productDataFeed === null) {
+      throw new Error(`TODO: Handle this`);
+    }
+
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=productdatafeed-${md5(productDataFeed)}.csv`,
+    );
+    res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
+    return res.send(productDataFeed);
+  } catch (err) {
+    res.status(400);
+    res.send(err);
+  } finally {
+    res.end();
   }
-
-  ctx.logger.info("Creating new product datafeed");
-
-  const productDataFeed = await generateProductDataFeed(
-    ctx.saleor.graphqlClient,
-    "FIXME: channel",
-    ctx.requestDataFeed.storefrontProductUrl,
-    ctx.requestDataFeed.variant,
-  );
-
-  if (productDataFeed === null) {
-    throw new Error(`TODO: Handle this`);
-  }
-
-  res.setHeader("Content-Type", "text/csv");
-  res.setHeader(
-    "Content-Disposition",
-    `attachment; filename=productdatafeed-${md5(productDataFeed)}.csv`,
-  );
-  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
-  return res.send(productDataFeed);
 }
