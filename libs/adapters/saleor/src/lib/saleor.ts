7c63d48a556a1f7198f39d063fc5c7e1a80c6ef6
Andreas Thomas <dev@chronark.com>
2021-08-24 17:50:12 +0200

    ci: docker

---
+++ libs/adapters/saleor/src/lib/saleor.ts
@@ -0,0 +1,55 @@
+import { Logger } from "tslog";
+
+export type SaleorWebHookReceiver = {
+  receive: (event: string) => Promise<void>;
+};
+export type SaleorServiceConfig = {
+  logger: Logger;
+};
+
+export class SaleorService implements SaleorWebHookReceiver {
+  private logger: Logger;
+  constructor(config: SaleorServiceConfig) {
+    this.logger = config.logger;
+  }
+
+  public async receive(event: string, ...middleware: any[]): Promise<void> {
+    // apply middleware
+
+    switch (event) {
+      case "customer_created":
+        return await this.onCustomerCreated();
+
+      case "order_created" || "order_confirmed":
+        return await this.onOrderCreatedOrConfirmed();
+
+      case "checkout_update":
+        return await this.onCheckoutUpdate();
+
+      case "order_fully_paid":
+        return await this.onOrderFullyPayed();
+
+      case "product_updated" || "product_created":
+        return await this.onProductCreatedOrUpdated();
+
+      default:
+        throw new Error(`Unknown event: ${event}`);
+    }
+  }
+
+  private async onCustomerCreated(): Promise<void> {
+    this.logger.fatal("IMPLEMENT ME");
+  }
+  private async onOrderCreatedOrConfirmed(): Promise<void> {
+    this.logger.fatal("IMPLEMENT ME");
+  }
+  private async onCheckoutUpdate(): Promise<void> {
+    this.logger.fatal("IMPLEMENT ME");
+  }
+  private async onOrderFullyPayed(): Promise<void> {
+    this.logger.fatal("IMPLEMENT ME");
+  }
+  private async onProductCreatedOrUpdated(): Promise<void> {
+    this.logger.fatal("IMPLEMENT ME");
+  }
+}
