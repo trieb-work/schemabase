7e4edfc232b0fc9e1fb394bb2899318fdeef920d
Andreas Thomas <dev@chronark.com>
2021-08-26 14:58:13 +0200

    ci: revert to services

--- apps/e2e/src/productdatafeed/generate.test.ts
+++ apps/e2e/src/productdatafeed/generate.test.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from "@eci/http";
-
+import { env } from "@eci/util/env";
 describe("productdatafeed", () => {
   it("generates a feed for facebook", async () => {
     const http = new HttpClient();
     const res = await http.call<string>({
       method: "GET",
-      url: "http://localhost:3000/api/product-data-feed/cksq51dwk00009ci06armhpsq?variant=facebookcommerce",
+      url: `${env.require(
+        "ECI_BASE_URL",
+      )}/api/product-data-feed/cksq51dwk00009ci06armhpsq?variant=facebookcommerce`,
     });
 
     expect(res.status).toBe(200);
