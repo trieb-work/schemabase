ca1079ef5fd03b461f8ee40588e1dd74cdddadcc
Jannik Zinkl <jannik@trieb.work>
2021-09-21 18:58:32 +0200

    feat: adding first class body

--- libs/integrations/strapi-orders-to-zoho/src/lib/integrations-strapi-orders-to-zoho.ts
+++ libs/integrations/strapi-orders-to-zoho/src/lib/integrations-strapi-orders-to-zoho.ts
@@ -1,7 +1,42 @@
+type StrapiEntryStandard = {
+  id: number,
+  created_at: string,
+  updated_at: string,
+}
 
+interface StrapiOrderEntry extends StrapiEntryStandard {
+  Status: string,
+  zohoCustomerId: string,
+}
 
-export function integrationsStrapiOrdersToZoho(): string {
-  return "integrations-strapi-orders-to-zoho";
+export type StrapiOrdersToZohoConfig = { 
+  event: "entry.update" | "entry.create",
+  created_at: string,
+  model: "auftrag",
+  entry: StrapiOrderEntry
+};
+export class integrationsStrapiOrdersToZoho {
+  public readonly entry: StrapiOrderEntry 
 
+  public readonly status: string
+
+  public constructor(config: StrapiOrdersToZohoConfig) {
+    this.entry = config.entry;
+    this.status = config.entry.Status;
+  }
+
+  public async createOrUpdate() {
+    switch (this.status) {
+      case "Draft":
+        
+        break;
+    
+      case "Confirmed":
+        break;  
+      default:
+        break;
+    }
+  };
+ 
 }
 
