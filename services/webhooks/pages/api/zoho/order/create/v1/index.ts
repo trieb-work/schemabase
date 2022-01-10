d1aa2abb98e3596b19d06fea147eaa5b105b37d0
Andreas Thomas <dev@chronark.com>
2022-01-10 16:27:14 +0100

    ci: fix deployment directory (#195)

--- services/webhooks/pages/api/zoho/order/create/v1/index.ts
+++ services/webhooks/pages/api/zoho/order/create/v1/index.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const logger = new Logger();
 
-  logger.info("req", { req: req.body });
+  logger.info("req", { req });
 
   res.json({ ok: true });
 };
