1fc3ae5c547a510e2a4d0c4f7ab7225b2150caf1
Andreas Thomas <dev@chronark.com>
2022-01-12 17:41:57 +0100

    chore: merge migrations

--- pkg/prisma/migrations/20220106134946_/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "TrackingEmailApp_tenantId_key";
--- pkg/prisma/migrations/20220112103733_add_email_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `sentEmailId` to the `TransactionalEmail` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TransactionalEmail" ADD COLUMN     "sentEmailId" TEXT NOT NULL;
