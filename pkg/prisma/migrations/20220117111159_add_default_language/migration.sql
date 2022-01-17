471416fdb77e9f31f0e4f2d0a31a9947982fec38
Andreas Thomas <dev@chronark.com>
2022-01-17 16:31:29 +0100

    chore: migrate (#215)

--- pkg/prisma/migrations/20220117111159_add_default_language/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `defaultLanguage` to the `TrackingEmailApp` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TrackingEmailApp" ADD COLUMN     "defaultLanguage" "Language" NOT NULL;
