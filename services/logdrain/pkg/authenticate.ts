21764b1eb5a5b00fc853ecd498e9b2046151bc7b
Andreas Thomas <dev@chronark.com>
2022-02-11 12:51:30 +0100

    fix: add logdrain secrets

--- services/logdrain/pkg/authenticate.ts
+++ services/logdrain/pkg/authenticate.ts
@@ -27,23 +27,3 @@ export async function authenticate(code: string): Promise<AuthToken> {
   }
   return (await res.json()) as AuthToken;
 }
-
-// {
-// "access_token": "EAhIuFNQPawVlUjVBtcTfffq",
-// "token_type": "Bearer",
-// "installation_id": "icfg_UHYjW73aMVyNoSOpeoHMQPEN",
-// "user_id": "Kxx65iI5fSCTOv2DIw7U5348",
-// "team_id": "team_DWmwXT06LmObdlKA94gALyfm"
-// }
-
-// {
-// "clientId": "oac_LtpUGj0js0vLIKFTxTru4TYw",
-// "configurationId": "icfg_UHYjW73aMVyNoSOpeoHMQPEN",
-// "createdAt": 1642780231375,
-// "id": "ld_EGYDKqfHMQfVhCfY",
-// "type": "json",
-// "name": "ngrok",
-// "ownerId": "team_DWmwXT06LmObdlKA94gALyfm",
-// "projectId": null,
-// "url": "https://e2a9-2003-ee-f705-2d00-5d66-c81-a1f1-d81.ngrok.io"
-// }
