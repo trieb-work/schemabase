7317d8d6d0a22eb680b6e1f6bb823f473512c9b6
Jannik Zinkl <jannik@trieb.work>
2022-05-29 18:28:18 +0200

    feat: krypto adding tests and description

--- pkg/krypto/README.md
+++ pkg/krypto/README.md
@@ -1,4 +1,11 @@
 # Encrypt/decrypt strings
 This package can be used to encrypt variables before storing them in the DB.
 
-It needs a secret key as env variable "SECRET_KEY" to work
+It needs a secret key as env variable "SECRET_KEY" to work.
+
+It uses the "cloak" package in the background: https://github.com/47ng/cloak
+
+The secret key needs to be of a specific format and can be generated easily with the cloak CLI:
+```
+pnpm cloak generate
+```
---
+++ pkg/krypto/src/krypto.test.ts
@@ -0,0 +1,21 @@
+import { describe, test, expect } from "@jest/globals";
+import { krypto } from "./krypto";
+
+describe("Test krypto module", () => {
+  const secretKey = "k1.aesgcm256.xgX2hMLPj4rBWwv-oUrKC_9pjpXPnKCnH4T35KcU-l8=";
+  const testString = "Test22";
+
+  let encryptedValue = "";
+  test("It should work to encrypt string", async () => {
+    const string = await krypto.encrypt(testString, secretKey);
+
+    expect(string).toBeDefined();
+    encryptedValue = string;
+  });
+
+  test("It should work to decrypt string", async () => {
+    const string = await krypto.decrypt(encryptedValue, secretKey);
+
+    expect(string).toBe(testString);
+  });
+});
