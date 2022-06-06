7049dd18cde312a454213544b01c22e55e642404
Jannik Zinkl <jannik@trieb.work>
2022-06-06 23:06:37 +0200

    fix: remove old migrations

--- pkg/prisma/OLD_migrations/20220126113023_optional_message_and_location/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "PackageEvent" ALTER COLUMN "message" DROP NOT NULL,
-ALTER COLUMN "location" DROP NOT NULL;
--- pkg/prisma/OLD_migrations/20220126150847_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "SendgridTemplate" ALTER COLUMN "packageState" DROP NOT NULL;
