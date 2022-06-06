e79cfa68d1c29366182877e3aabdd5c272c533f1
Jannik Zinkl <jannik@trieb.work>
2022-06-06 23:06:52 +0200

    feat: adding prisma seeding file

---
+++ pkg/prisma/seeding/testing.ts
@@ -0,0 +1,77 @@
+import { PrismaClient } from "..";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.tenant.create({
+    data: {
+      id: "test",
+      name: "test",
+    },
+  });
+  await prisma.saleorApp.create({
+    data: {
+      id: "test",
+      name: "test",
+      domain: "test",
+      tenant: {
+        connect: {
+          id: "test",
+        },
+      },
+    },
+  });
+  await prisma.installedSaleorApp.create({
+    data: {
+      id: "test",
+      token: "test",
+      saleorApp: {
+        connect: {
+          id: "test",
+        },
+      },
+    },
+  });
+  await prisma.zohoApp.create({
+    data: {
+      id: "test",
+      orgId: "test",
+      clientId: "test",
+      clientSecret: "test",
+      tenant: {
+        connect: {
+          id: "test",
+        },
+      },
+    },
+  });
+  await prisma.saleorZohoIntegration.create({
+    data: {
+      id: "test",
+      tenant: {
+        connect: {
+          id: "test",
+        },
+      },
+      zohoApp: {
+        connect: {
+          id: "test",
+        },
+      },
+      installedSaleorApp: {
+        connect: {
+          id: "test",
+        },
+      },
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
