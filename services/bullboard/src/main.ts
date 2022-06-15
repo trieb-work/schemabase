070dcabab8079b71d957659e9a587c312cece7cc
Jannik Zinkl <jannik@trieb.work>
2022-06-15 15:14:59 +0200

    feat: handling shutdown signals

--- services/bullboard/src/main.ts
+++ services/bullboard/src/main.ts
@@ -110,8 +110,31 @@ async function main() {
     res.redirect("/ui");
   });
 
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.log(`Running on port ${port}...`);
+  });
+
+  // The signals we want to handle
+  // NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+  const signals: { [key: string]: number } = {
+    SIGHUP: 1,
+    SIGINT: 2,
+    SIGTERM: 15,
+  };
+  // Do any necessary shutdown logic for our application here
+  const shutdown = (signal: any, value: any) => {
+    console.log("shutdown!");
+    server.close(() => {
+      console.log(`server stopped by ${signal} with value ${value}`);
+      process.exit(128 + value);
+    });
+  };
+  // Create a listener for each of the signals that we want to handle
+  Object.keys(signals).forEach((signal) => {
+    process.on(signal, () => {
+      console.log(`process received a ${signal} signal`);
+      shutdown(signal, signals[signal]);
+    });
   });
 }
 
