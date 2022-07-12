a7c16e1fe30f8421ac3e78e9d5679e4efea1acc5
Jannik Zinkl <jannik@trieb.work>
2022-07-12 17:11:45 +0200

    feat: try catch for product upsert

--- pkg/integration-zoho-entities/src/items.ts
+++ pkg/integration-zoho-entities/src/items.ts
@@ -40,77 +40,91 @@ export class ZohoItemSyncService {
     // Loop through every item and upsert the corresponding
     // product, productVariant and ZohoItem in the DB
     for (const item of items) {
-      await this.db.productVariant.upsert({
-        where: {
-          sku_tenantId: {
-            tenantId,
-            sku: item.sku,
+      try {
+        await this.db.productVariant.upsert({
+          where: {
+            sku_tenantId: {
+              tenantId,
+              sku: item.sku,
+            },
           },
-        },
-        update: {
-          zohoItem: {
-            upsert: {
-              where: {
-                id_zohoAppId: {
+          update: {
+            zohoItem: {
+              upsert: {
+                where: {
+                  id_zohoAppId: {
+                    id: item.item_id,
+                    zohoAppId: this.zohoApp.id,
+                  },
+                },
+                create: {
                   id: item.item_id,
                   zohoAppId: this.zohoApp.id,
+                  createdAt: new Date(item.created_time),
+                  updatedAt: new Date(item.last_modified_time),
+                },
+                update: {
+                  createdAt: new Date(item.created_time),
+                  updatedAt: new Date(item.last_modified_time),
                 },
               },
+            },
+          },
+          create: {
+            id: id.id("variant"),
+            sku: item.sku,
+            tenant: {
+              connect: {
+                id: tenantId,
+              },
+            },
+            zohoItem: {
               create: {
                 id: item.item_id,
+                createdAt: new Date(item.created_time),
+                updatedAt: new Date(item.last_modified_time),
                 zohoAppId: this.zohoApp.id,
-                createdAt: new Date(item.created_time),
-                updatedAt: new Date(item.last_modified_time),
-              },
-              update: {
-                createdAt: new Date(item.created_time),
-                updatedAt: new Date(item.last_modified_time),
               },
             },
-          },
-        },
-        create: {
-          id: id.id("variant"),
-          sku: item.sku,
-          tenant: {
-            connect: {
-              id: tenantId,
-            },
-          },
-          zohoItem: {
-            create: {
-              id: item.item_id,
-              createdAt: new Date(item.created_time),
-              updatedAt: new Date(item.last_modified_time),
-              zohoAppId: this.zohoApp.id,
-            },
-          },
-          // It is a product variant, if we have a "group_name" in Zoho.
-          // In this moment, we first have to check, if a product does already exist using the
-          // group_name (which is actually a product name)
-          product: {
-            connectOrCreate: {
-              where: {
-                normalizedName_tenantId: {
+            // It is a product variant, if we have a "group_name" in Zoho.
+            // In this moment, we first have to check, if a product does already exist using the
+            // group_name (which is actually a product name)
+            product: {
+              connectOrCreate: {
+                where: {
+                  normalizedName_tenantId: {
+                    tenantId,
+                    normalizedName: normalizeStrings.productNames(
+                      item?.group_name || "",
+                    ),
+                  },
+                },
+                create: {
+                  id: id.id("product"),
                   tenantId,
+                  // If this is a single variant product, we set the variant name as
+                  // the product name
+                  name: item?.group_name || item.name,
                   normalizedName: normalizeStrings.productNames(
-                    item?.group_name || "",
+                    item?.group_name || item.name,
                   ),
                 },
               },
-              create: {
-                id: id.id("product"),
-                tenantId,
-                // If this is a single variant product, we set the variant name as the product name
-                name: item?.group_name || item.name,
-                normalizedName: normalizeStrings.productNames(
-                  item?.group_name || item.name,
-                ),
-              },
             },
           },
-        },
-      });
+        });
+      } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          if (e.code === "P2002") {
+            this.logger.error(
+              `Prisma unique constrained failed for product ${item.item_id} - SKU ${item.sku}`,
+            );
+          }
+        } else {
+          this.logger.error(JSON.stringify(e));
+          continue;
+        }
+      }
     }
     this.logger.info(`Sync finished for ${items.length} Zoho Items`);
   }
