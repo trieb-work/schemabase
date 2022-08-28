0e175833490d5bf7ce81810e65743290a1ca643d
Jannik Zinkl <jannik@trieb.work>
2022-08-28 20:37:15 +0200

    style: format

--- pkg/integration-zoho-entities/src/salesorders/address.ts
+++ pkg/integration-zoho-entities/src/salesorders/address.ts
@@ -2,15 +2,22 @@ import { Address, ZohoAddress } from "@prisma/client";
 import { Warning } from ".";
 
 type AddressWithZohoAddress = Address & {
-    zohoAddress: ZohoAddress[];
-}
+  zohoAddress: ZohoAddress[];
+};
 
-export function addressToZohoAddressId(address: AddressWithZohoAddress): string {
-    if(!address?.zohoAddress || address.zohoAddress.length === 0){
-        throw new Warning("No zohoAddress set for the address (shipping or billing) of this order. Aborting sync of this order. Try again after zoho address sync.");
-    }
-    if(address.zohoAddress.length > 1){
-        throw new Error("Multiple zohoAddresses set for the address of this order. Aborting sync of this order.");
-    }
-    return address.zohoAddress[0].id;
+export function addressToZohoAddressId(
+  address: AddressWithZohoAddress,
+): string {
+  if (!address?.zohoAddress || address.zohoAddress.length === 0) {
+    throw new Warning(
+      // eslint-disable-next-line max-len
+      "No zohoAddress set for the address (shipping or billing) of this order. Aborting sync of this order. Try again after zoho address sync.",
+    );
+  }
+  if (address.zohoAddress.length > 1) {
+    throw new Error(
+      "Multiple zohoAddresses set for the address of this order. Aborting sync of this order.",
+    );
+  }
+  return address.zohoAddress[0].id;
 }
