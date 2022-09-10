9012b7372dc570b01c52452f1ec519d219e82f72
Jannik Zinkl <jannik@trieb.work>
2022-09-10 22:19:58 +0200

    fix: trim is needed dont know why

--- pkg/braintree/src/braintree.test.ts
+++ pkg/braintree/src/braintree.test.ts
@@ -1,27 +1,28 @@
-// import { PrismaClient } from "@eci/pkg/prisma";
+import { PrismaClient } from "@eci/pkg/prisma";
 import { BraintreeTransaction, BraintreeClient } from "./braintree";
 import { describe, test, beforeAll, expect } from "@jest/globals";
+import { krypto } from "@eci/pkg/krypto";
 
-// const prismaClient = new PrismaClient();
+const prismaClient = new PrismaClient();
 let braintree: BraintreeClient;
-
-const merchantIdTest = "83xhsc8ghngkhp7q";
-const publicKeyTest = "f66g4gs2pcqxx3rx";
-const privateKeyTest = "6d576b42ff5cd9e89a2f384fad67f5cf";
 
 describe("Braintree Class Unit Test", () => {
   beforeAll(async () => {
-    // const braintreeConf = await prismaClient.braintree.findUnique({
-    //   where: {
-    //     id: "test",
-    //   },
-    // });
-    // if (!braintreeConf || !braintreeConf.merchantId)
-    //   throw new Error("No braintree config with id `test` found!");
+    const braintreeConf = await prismaClient.braintreeApp.findUnique({
+      where: {
+        id: "test",
+      },
+    });
+    if (!braintreeConf || !braintreeConf.merchantId)
+      throw new Error("No braintree config with id `test` found!");
+    const merchantId = await krypto.decrypt(braintreeConf.merchantId);
+    const privateKey = await krypto.decrypt(braintreeConf.privateKey);
+    const publicKey = await krypto.decrypt(braintreeConf.publicKey);
+
     braintree = new BraintreeClient({
-      merchantId: merchantIdTest,
-      privateKey: privateKeyTest,
-      publicKey: publicKeyTest,
+      merchantId,
+      privateKey,
+      publicKey,
       sandbox: true,
     });
   });
