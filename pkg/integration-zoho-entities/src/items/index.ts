100ee79f9030f7dad70cc2dda5b0895f70077eed
Jannik Zinkl <jannik@trieb.work>
2022-09-19 17:33:09 +0200

    fix: product use item name not only group name

--- pkg/integration-zoho-entities/src/items/index.ts
+++ pkg/integration-zoho-entities/src/items/index.ts
@@ -174,7 +174,7 @@ export class ZohoItemSyncService {
                         normalizedName_tenantId: {
                           tenantId,
                           normalizedName: normalizeStrings.productNames(
-                            item?.group_name || "",
+                            item?.group_name || item.name,
                           ),
                         },
                       },
