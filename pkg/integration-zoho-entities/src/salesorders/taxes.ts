ead785bc5cabf3519a6fdffa05acfa494ca371ad
Jannik Zinkl <jannik@trieb.work>
2023-01-22 12:49:46 +0100

    feat: prepare for better tax matching

--- pkg/integration-zoho-entities/src/salesorders/taxes.ts
+++ pkg/integration-zoho-entities/src/salesorders/taxes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { ZohoTax, Tax } from "@prisma/client";
 import { Warning } from "../utils";
 
@@ -13,7 +14,7 @@ export function taxToZohoTaxId(tax: ExtendedTax): string {
   }
   if (tax.zohoTaxes.length > 1) {
     throw new Error(
-      "Multiple zohoTaxes set for this tax. Aborting sync of this order.",
+      `Multiple zohoTaxes set for this tax. Percentage:${tax.percentage}. Aborting sync of this order.`,
     );
   }
   return tax.zohoTaxes[0].id;
