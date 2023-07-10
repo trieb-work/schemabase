4e71c37bd77497afc31e7c5ee581a826c83b7fdf
Jannik Zinkl <jannik@trieb.work>
2023-07-10 18:07:42 +0200

    fix: syntax fix

--- pkg/integration-kencove-api/src/addresses.ts
+++ pkg/integration-kencove-api/src/addresses.ts
@@ -13,51 +13,44 @@ interface KencoveApiAppAddressSyncServiceConfig {
 }
 
 export class KencoveApiAppAddressSyncService {
-    private readonly logger: ILogger;
+  private readonly logger: ILogger;
 
-    private readonly db: PrismaClient;
+  private readonly db: PrismaClient;
 
-    public readonly kencoveApiApp: KencoveApiApp;
+  public readonly kencoveApiApp: KencoveApiApp;
 
-    private readonly cronState: CronStateHandler;
+  private readonly cronState: CronStateHandler;
 
+  public constructor(config: KencoveApiAppAddressSyncServiceConfig) {
+    this.logger = config.logger;
+    this.db = config.db;
+    this.kencoveApiApp = config.kencoveApiApp;
+    this.cronState = new CronStateHandler({
+      tenantId: this.kencoveApiApp.tenantId,
+      appId: this.kencoveApiApp.id,
+      db: this.db,
+      syncEntity: "addresses",
+    });
+  }
 
-    public constructor(config: KencoveApiAppAddressSyncServiceConfig) {
-        this.logger = config.logger;
-        this.db = config.db;
-        this.kencoveApiApp = config.kencoveApiApp;
-        this.cronState = new CronStateHandler({
-            tenantId: this.kencoveApiApp.tenantId,
-            appId: this.kencoveApiApp.id,
-            db: this.db,
-            syncEntity: "addresses",
-          });
-        }
+  public async syncToEci() {
+    const cronState = await this.cronState.get();
+    const now = new Date();
+    let createdGte: Date;
+    if (!cronState.lastRun) {
+      createdGte = subYears(now, 1);
+      this.logger.info(
+        // eslint-disable-next-line max-len
+        `This seems to be our first sync run. Syncing data from: ${createdGte}`,
+      );
+    } else {
+      // for security purposes, we sync one hour more than the last run
+      createdGte = subHours(cronState.lastRun, 1);
+      this.logger.info(`Setting GTE date to ${createdGte}.`);
+    }
 
-    public async syncToEci() {
-        // get the cron state from the database and use it to get the fromDate needed for the getAddresses method
-        const cronState = await this.cronState.get();
-        const now = new Date();
-        let createdGte: Date;
-        if (!cronState.lastRun) {
-          createdGte = subYears(now, 1);
-          this.logger.info(
-            // eslint-disable-next-line max-len
-            `This seems to be our first sync run. Syncing data from: ${createdGte}`,
-          );
-        } else {
-          // for security purposes, we sync one hour more than the last run
-          createdGte = subHours(cronState.lastRun, 1);
-          this.logger.info(
-            `Setting GTE date to ${createdGte}.`,
-          );
-        }
-
-        const client = new KencoveApiClient(this.kencoveApiApp);
-        const addresses = await client.getAddresses(createdGte);
-        this.logger.info(`Found ${addresses.length} addresses to sync`);
-        // run a loop for each address and update or create the address in the database. Only update, if the address is different than the one in the database.
-
-
-
+    const client = new KencoveApiClient(this.kencoveApiApp);
+    const addresses = await client.getAddresses(createdGte);
+    this.logger.info(`Found ${addresses.length} addresses to sync`);
+  }
 }
