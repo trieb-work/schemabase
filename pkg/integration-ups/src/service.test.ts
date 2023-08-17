b1a5ab588c3c269a525b549fe8dd8c2f6a525898
Jannik Zinkl <jannik@trieb.work>
2023-08-17 20:14:58 +0200

    chore: ups test

--- pkg/integration-ups/src/service.test.ts
+++ pkg/integration-ups/src/service.test.ts
@@ -1,14 +1,40 @@
 import { describe, test } from "@jest/globals";
-
+import { UPSTrackingSyncService } from "./service";
+import { PrismaClient } from "@eci/pkg/prisma";
+import { AssertionLogger } from "@eci/pkg/logger";
 
 describe("UPS package sync test", () => {
   // make sure, we have a UPS testing package
 
-  // testing the ups service app. Mocking the DB client. Using real UPS client id and secret for testing
+  // testing the ups service app. Use the UPS account with id "test"
   test("Test client", async () => {
-  })
+    const db = new PrismaClient();
 
+    const upsTrackingApp = await db.uPSTrackingApp.findUniqueOrThrow({
+      where: {
+        id: "test",
+      },
+    });
 
+    await db.package.upsert({
+      where: {
+        id: "test",
+      },
+      create: {
+        id: "test",
+        tenantId: "test",
+        carrier: "UPS",
+        trackingId: "1Z2632010391767531",
+        number: "test",
+      },
+      update: {},
+    });
 
-    
+    const client = new UPSTrackingSyncService({
+      db,
+      logger: new AssertionLogger(),
+      upsTrackingApp,
+    });
+    await client.syncToECI();
+  });
 });
