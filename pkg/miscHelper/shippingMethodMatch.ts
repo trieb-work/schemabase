bb2bac293ff07689546a6e86503655a83c0c2254
Jannik Zinkl <jannik@trieb.work>
2023-08-21 20:45:12 +0200

    fix: new carrier in generic carrier match

--- pkg/miscHelper/shippingMethodMatch.ts
+++ pkg/miscHelper/shippingMethodMatch.ts
@@ -12,6 +12,9 @@ export const shippingMethodMatch = (carrierString: string): Carrier => {
   if (carrierLowercase.includes("dhl")) return Carrier.DHL;
   if (carrierLowercase.includes("ups")) return Carrier.UPS;
   if (carrierLowercase.includes("abholung")) return Carrier.PICKUP;
+  if (carrierLowercase.includes("hermes")) return Carrier.HERMES;
+  if (carrierLowercase.includes("usps")) return Carrier.USPS;
+  if (carrierLowercase.includes("fedex")) return Carrier.FEDEX;
   if (carrierLowercase.includes("bulk")) return Carrier.BULK;
   return Carrier.UNKNOWN;
 };
