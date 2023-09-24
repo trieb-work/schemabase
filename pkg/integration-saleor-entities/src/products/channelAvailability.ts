5f80ee065a45984fee2ed6b2fe16cb3dbd58ceca
Jannik Zinkl <jannik@trieb.work>
2023-09-24 18:51:04 +0200

    fix: tenant id and minor error handling

--- pkg/integration-saleor-entities/src/products/channelAvailability.ts
+++ pkg/integration-saleor-entities/src/products/channelAvailability.ts
@@ -11,6 +11,8 @@ export class ChannelAvailability {
 
     private readonly installedSaleorAppId: string;
 
+    private readonly tenantId: string;
+
     private readonly logger: ILogger;
 
     private readonly saleorClient: SaleorClient;
@@ -20,9 +22,11 @@ export class ChannelAvailability {
         installedSaleorAppId: string,
         logger: ILogger,
         saleorClient: SaleorClient,
+        tenantId: string,
     ) {
         this.db = db;
         this.installedSaleorAppId = installedSaleorAppId;
+        this.tenantId = tenantId;
         this.logger = logger;
         this.saleorClient = saleorClient;
     }
@@ -30,7 +34,7 @@ export class ChannelAvailability {
     public async syncChannelAvailability(gteDate: Date) {
         const channelPricings = await this.db.salesChannelPriceEntry.findMany({
             where: {
-                tenantId: this.installedSaleorAppId,
+                tenantId: this.tenantId,
                 updatedAt: {
                     gte: gteDate,
                 },
@@ -112,7 +116,7 @@ export class ChannelAvailability {
                 );
                 continue;
             }
-            await this.saleorClient.productChannelListingUpdate({
+            const resp1 = await this.saleorClient.productChannelListingUpdate({
                 id: saleorProductId,
                 input: {
                     updateChannels: [
@@ -120,19 +124,50 @@ export class ChannelAvailability {
                             availableForPurchaseAt: entry.startDate,
                             channelId: entry.salesChannel.saleorChannels[0].id,
                             visibleInListings: true,
+                            isAvailableForPurchase: true,
+                            isPublished: true,
                         },
                     ],
                 },
             });
-            await this.saleorClient.productVariantChannelListingUpdate({
-                id: entry.productVariant.saleorProductVariant[0].id,
-                input: {
-                    channelId: entry.salesChannel.saleorChannels[0].id,
-                    price: {
-                        amount: entry.price,
-                    },
-                },
-            });
+
+            if (
+                resp1.productChannelListingUpdate?.errors &&
+                resp1.productChannelListingUpdate?.errors.length > 0
+            ) {
+                this.logger.error(
+                    `Error updating product channel availability for product variant ${
+                        entry.productVariant.id
+                    }: ${JSON.stringify(
+                        resp1.productChannelListingUpdate.errors,
+                    )}`,
+                );
+                continue;
+            }
+
+            const resp2 =
+                await this.saleorClient.productVariantChannelListingUpdate({
+                    id: entry.productVariant.saleorProductVariant[0].id,
+                    input: [
+                        {
+                            channelId: entry.salesChannel.saleorChannels[0].id,
+                            price: entry.price,
+                        },
+                    ],
+                });
+            if (
+                resp2.productVariantChannelListingUpdate?.errors &&
+                resp2.productVariantChannelListingUpdate?.errors.length > 0
+            ) {
+                this.logger.error(
+                    `Error updating product variant channel price for product variant ${
+                        entry.productVariant.id
+                    }: ${JSON.stringify(
+                        resp2.productVariantChannelListingUpdate.errors,
+                    )}`,
+                );
+                continue;
+            }
         }
     }
 }
