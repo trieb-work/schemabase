0b70831fe7cba1911be45efa50eb1231cc558689
Jannik Zinkl <jannik@trieb.work>
2024-02-21 16:27:39 +0100

    feat: compare array

--- pkg/utils/array.ts
+++ pkg/utils/array.ts
@@ -7,3 +7,11 @@ export async function arrayFromAsyncGenerator<T>(
     }
     return items;
 }
+
+/**
+ * Just compare two arrays with each other using sort and stringify.
+ * Return true, if the arrays entries are equal, otherwise false.
+ */
+export function compareArrays<T>(a: T[], b: T[]): boolean {
+    return JSON.stringify(a.sort()) === JSON.stringify(b.sort());
+}
