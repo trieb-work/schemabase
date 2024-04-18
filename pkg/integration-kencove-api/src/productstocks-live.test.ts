f710f48d03f0c0030ce74360cd0a13d1abaa8ac8
Jannik Zinkl <jannik@trieb.work>
2024-04-18 14:43:19 +0200

    chore: test for productstock

---
+++ pkg/integration-kencove-api/src/productstocks-live.test.ts
@@ -0,0 +1,32 @@
+import { AssertionLogger } from "@eci/pkg/logger";
+import { PrismaClient } from "@eci/pkg/prisma";
+import { beforeEach, describe, jest, test } from "@jest/globals";
+import "@eci/pkg/jest-utils/consoleFormatter";
+import { KencoveApiAppProductStockSyncService } from "./productstocks";
+
+/// Use this file to locally run this service
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+describe("Kencove product Test", () => {
+    const prismaClient = new PrismaClient();
+
+    test("It should work to sync product stocks", async () => {
+        const kencoveApiApp = await prismaClient.kencoveApiApp.findUnique({
+            where: {
+                id: "kencove_prod",
+                // id: "ken_app_prod",
+            },
+        });
+        if (!kencoveApiApp) throw new Error("Kencove Api App not found in DB");
+
+        const service = new KencoveApiAppProductStockSyncService({
+            kencoveApiApp,
+            logger: new AssertionLogger(),
+            db: prismaClient,
+        });
+        await service.syncToEci("5961");
+    }, 10000000);
+});
