af11a827e6859f1e9b5bd561c0e3c84842d6f524
Jannik Zinkl <jannik@trieb.work>
2024-07-29 15:12:57 +0200

    fix: reference number can be null

--- pkg/integration-kencove-api/src/types.d.ts
+++ pkg/integration-kencove-api/src/types.d.ts
@@ -332,7 +332,7 @@ export type KencoveApiPayment = {
     sale_order_id: number;
     sale_order_number: string;
     move_type: string;
-    acquirer_reference: string;
+    acquirer_reference: string | null;
     payment_method: string;
     payment_amount: number;
     payment_state: "done" | "refunded";
