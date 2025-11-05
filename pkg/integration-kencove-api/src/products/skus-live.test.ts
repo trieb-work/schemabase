c301c0e4ea0a4c1437cde54fdf740704d197988e
Jannik Zinkl <jannik@trieb.work>
2025-11-05 23:43:12 +0100

    feat: adding test case

---
+++ pkg/integration-kencove-api/src/products/skus-live.test.ts
@@ -0,0 +1,32 @@
+import { AssertionLogger } from "@eci/pkg/logger";
+import { PrismaClient } from "@eci/pkg/prisma";
+import { beforeEach, describe, jest, test } from "@jest/globals";
+import "@eci/pkg/jest-utils/consoleFormatter";
+import { KencoveApiAppProductSkuSyncService } from "./skus";
+
+/// Use this file to locally run this service
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+describe("Kencove product Test", () => {
+    const prismaClient = new PrismaClient();
+
+    test("It should work to sync products", async () => {
+        const kencoveApiApp = await prismaClient.kencoveApiApp.findUnique({
+            where: {
+                // id: "kencove_prod",
+                id: "ken_app_prod",
+            },
+        });
+        if (!kencoveApiApp) throw new Error("Kencove Api App not found in DB");
+
+        const service = new KencoveApiAppProductSkuSyncService({
+            kencoveApiApp,
+            logger: new AssertionLogger(),
+            db: prismaClient,
+        });
+        await service.syncToECI();
+    }, 10000000);
+});
